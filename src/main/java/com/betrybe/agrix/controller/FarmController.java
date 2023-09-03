c1e9df48269ccb023dcc13aa764a5a65f67dc5be
Unidracode <unidragorn@gmail.com>
2023-09-03 11:48:44 -0300

    requisito 2

--- src/main/java/com/betrybe/agrix/controller/FarmController.java
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -3,9 +3,12 @@ package com.betrybe.agrix.controllers;
 import com.betrybe.agrix.dto.FarmDto;
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.service.FarmService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,10 +31,19 @@ public class FarmController {
    * comment.
    */
 
-  @RequestMapping
+  @PostMapping
   public ResponseEntity<Farm> createFarm(@RequestBody FarmDto farmDto) {
     Farm newFarm = farmService.insertFarm(farmDto.toFarm());
-
     return ResponseEntity.status(HttpStatus.CREATED).body(newFarm);
+  }
+
+  /**
+   * comment.
+   */
+
+  @GetMapping
+  public ResponseEntity<List<Farm>> getAllFarms() {
+    List<Farm> farms = farmService.getAllFarms();
+    return ResponseEntity.ok().body(farms);
   }
 }
